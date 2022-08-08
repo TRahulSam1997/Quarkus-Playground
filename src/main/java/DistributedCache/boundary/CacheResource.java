10b880befcbe89d07397872b3ab1c6ad541b9ece
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-08 10:39:13 +1000

    cached response endpoint added

---
+++ src/main/java/DistributedCache/boundary/CacheResource.java
@@ -0,0 +1,21 @@
+package DistributedCache.boundary;
+
+import DistributedCache.interceptor.Cached;
+import io.smallrye.mutiny.Uni;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/services")
+public class CacheResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{id}")
+    @Cached(cacheName = "my-cache")
+    public Uni<String> getServiceValue(@PathParam("id") Long path) {
+        return Uni.createFrom().item("CacheItemValue");
+    }
+}
