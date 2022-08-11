04df044135b8867381143e520a6910c09ec7780f
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-11 12:41:39 +1000

    Decorator added

--- src/main/java/Beans/Decorators/LargeTxAccount.java
+++ src/main/java/Beans/Decorators/LargeTxAccount.java
@@ -1,4 +1,31 @@
 package Beans.Decorators;
 
-public class LargeTxAccount {
+import javax.annotation.Priority;
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import java.math.BigDecimal;
+
+interface Account {
+    void withdraw(BigDecimal amount);
+}
+
+@Priority(10)
+@Decorator
+public class LargeTxAccount implements Account {
+    @Inject
+    @Any
+    @Delegate
+    Account delegate;
+
+    @Inject
+    LogService logService;
+
+    public void withdraw(BigDecimal amount) {
+        delegate.withdraw(amount);
+        if(amount.compareTo(BigDecimal.valueOf(1000)) > 0) {
+            logService.logWithdrawal(delegate, amount);
+        }
+    }
 }
---
+++ src/main/java/Beans/Decorators/LogService.java
@@ -0,0 +1,8 @@
+package Beans.Decorators;
+
+import java.math.BigDecimal;
+
+public class LogService {
+    public void logWithdrawal(Account delegate, BigDecimal amount) {
+    }
+}
